b6746d1f332d7c7ba6a3c49273bdd9436a6938fd
Mouhebnaddari <92733296+Mouhebnaddari@users.noreply.github.com>
2022-05-21 12:17:47 +0200

    cron-generator

--- src/scheduling.js
+++ src/scheduling.js
@@ -2,13 +2,67 @@ import 'antd/dist/antd.min.css'
 import './App.css';
 import {Content} from "antd/lib/layout/layout";
 import "@fontsource/ubuntu-mono";
+import {Button, Form, Input} from "antd";
+import {useState} from "react";
+import React  from 'react'
+import Cron  from 'react-cron-generator'
+import 'react-cron-generator/dist/cron-builder.css'
+
+
+
+
+
 
 export default function Scheduling() {
 
-    return (
+    const [job, setJob] = useState("")
+    const [remote, setRemote] = useState("")
+    const [isLoading, setLoading] = useState(false)
+    const [value,setValue] = useState('* * * * * * *')
 
+    const [form] = Form.useForm();
+    const submit = async () => {
+        setLoading(true);
+        setLoading(false);
+        form.resetFields()
+    }
+
+    return (
+<div>
                 <Content className="site-layout-content">
-                    todo
+                    <Form labelCol={{span: 8}} wrapperCol={{span: 10}}  form={form} onFinish={submit}>
+                        <Form.Item
+                            label="Job"
+                            name="Job"
+                            rules={[{required: true, message: 'Please input the job!',}]}
+                            value={job}
+                            onChange={(e => setJob(e.target.value))}
+                        >
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item
+                            label="Remote"
+                            name="Remote"
+                            rules={[{required: true, message: 'Please input your Remote!'}]}
+                            value={remote}
+                            onChange={(e => setRemote(e.target.value))}
+                        >
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item wrapperCol={{offset: 12, span: 20}}>
+                            <Button type="primary" loading={isLoading} htmlType='submit'>
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                    <Cron
+                        onChange={(e)=> {setValue({value:e});}}
+                        value={value}
+                        showResultText={true}
+                        showResultCron={true}
+                    />
                 </Content>
+</div>
+
     );
 };
--- src/services/jobService.js
+++ src/services/jobService.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const runAPI = '/run'
 const getHistoryAPI = '/getHistory'
+const schedulingAPI ='/scheduling'
 
 
 export async function call(job, hostname) {
@@ -13,6 +14,11 @@ export async function call(job, hostname) {
 
 export async function getHistory() {
     return axios.get(getHistoryAPI)
+        .then(res => res.data)
+        .catch(err => console.error(err))
+}
+export async function schedule(job,hostname,value) {
+    return axios.get(schedulingAPI,{params:{job,hostname,value}})
         .then(res => res.data)
         .catch(err => console.error(err))
 }
